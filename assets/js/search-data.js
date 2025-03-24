48580b46c674f868b8d552d417767588d5db102c
naranggagan <narang.gagan@outlook.com>
2025-03-24 23:41:02 +0000

    Deploying to gh-pages from @ naranggagan/naranggagan.github.io@5371c096cf45306c4ed778862d289b148ffd4b2b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -490,7 +490,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%67.%6E%61%72%61%6E%67@%70%6D.%75%6E%69%76%70%6D.%69%74", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -511,14 +511,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=88ILtB4AAAAJ&hl", "_blank");
         },
       },{
       id: 'light-theme',
